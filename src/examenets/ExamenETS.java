26127c7ac7691858d2f197679644f1d7dfb38a0e
julia <julia@Julia>
2019-06-05 17:40:01 +0100

    Examen variables metidas por JOpyionPane

---
+++ src/examenets/ExamenETS.java
@@ -0,0 +1,61 @@
+package examenets;
+
+/**
+ * @author julia
+ */
+public class ExamenETS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int sum1;
+        String suma1 = "";
+        sum1 = Integer.parseInt(suma1);
+        int sum2;
+        String suma2 = "";
+        sum2 = Integer.parseInt(suma2);
+        int sum3;
+        String suma3 = "";
+        sum3 = Integer.parseInt(suma3);
+        int sumar = sum1 + sum2 + sum3;
+        System.out.println("Operación Suma = " + sumar);
+
+        int rest1;
+        String resta1 = "";
+        rest1 = Integer.parseInt(resta1);
+        int rest2;
+        String resta2 = "";
+        rest2 = Integer.parseInt(resta2);
+        int rest3;
+        String resta3 = "";
+        rest3 = Integer.parseInt(resta3);
+        int restar = rest1 - rest2 - rest3;
+        System.out.println("Operación Restar =  " + restar);
+
+        double mul1;
+        String multi1 = "";
+        mul1 = Double.parseDouble(multi1);
+        double mul2;
+        String multi2 = "";
+        mul2 = Double.parseDouble(multi2);
+        double mul3;
+        String multi3 = "";
+        mul3 = Double.parseDouble(multi3);
+        double multiplicar = mul1 * mul2 * mul3;
+        System.out.println("Operación Multiplicar = " + multiplicar);
+
+        double div1;
+        String divi1 = "";
+        div1 = Double.parseDouble(divi1);
+        double div2;
+        String divi2 = "";
+        div2 = Double.parseDouble(divi2);
+        double div3;
+        String divi3 = "";
+        div3 = Double.parseDouble(divi3);
+        double dividir = div1 / div2 / div3;
+        System.out.println("Operación dividir = " + dividir);
+    }
+
+}
